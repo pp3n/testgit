146034e4e1fd58b9503e38fe2326ddddc45b43da
pp3n <pp3n@protonmail.ch>
2016-04-25 22:13:26 -0400

    Final steps

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-	validates :title, presence: true, length: {minimum: 5, maximum: 10}
+	validates :title, presence: true, length: {minimum: 5, maximum: 20}
 	validates :description, presence:true, length: {minimum: 10, maximum: 300}
 
 
